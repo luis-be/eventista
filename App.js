10c5a5e19230e73a4b96cb2a599a06b7d0d51029
Casiano Benitez <luis.benitez.ayala@gmail.com>
2023-04-17 23:16:53 -0400

    asdasdasd

--- App.js
+++ App.js
@@ -4,9 +4,18 @@ import tw from "tailwind-rn";
 
 export default function App() {
   return (
-    <View style={tw("justify-center items-center")}>
+    <View style={styles.container}>
       <Text>asdvasdasdcasdcasd</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
